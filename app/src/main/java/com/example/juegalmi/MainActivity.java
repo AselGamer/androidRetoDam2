68d147c42185d9eb53d30a6e74c5dc4668e01660
Iraitz7 <iraitzmauleon@gmail.com>
2023-10-25 23:52:32 +0200

    commit de tarde

--- app/src/main/java/com/example/juegalmi/MainActivity.java
+++ app/src/main/java/com/example/juegalmi/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.juegalmi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.example.juegalmi.adaptadores.PagerAdaptador;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    TabItem tabInterior, tabExterior;
+    PagerAdaptador pagerAdaptador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+/*
+        //Aprovechamos para recoger(instanciar) los botones y demas
+        tabLayout = findViewById(R.id.tabLayoutAbajo);
+        viewPager = findViewById(R.id.pager);
+        tabInterior = findViewById(R.id.tabInterior);
+        tabExterior = findViewById(R.id.tabExterior);
+
+        pagerAdaptador = new PagerAdaptador(getFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdaptador);*/
     }
 }
---
+++ app/src/main/java/com/example/juegalmi/adaptadores/PagerAdaptador.java
@@ -0,0 +1,44 @@
+package com.example.juegalmi.adaptadores;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.juegalmi.tabsAbajo.Galeria;
+import com.example.juegalmi.tabsAbajo.Productos;
+import com.example.juegalmi.tabsAbajo.Reparaciones;
+import com.example.juegalmi.tabsAbajo.SobreNosotros;
+import com.example.juegalmi.tabsAbajo.Ubicacion;
+
+public class PagerAdaptador extends FragmentPagerAdapter {
+    int numTabs;
+
+    public PagerAdaptador(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+        this.numTabs = behavior;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        if(position == 0){
+            return new Productos();
+        }else if(position == 1){
+            return new Reparaciones();
+        }else if(position == 2){
+            return new Ubicacion();
+        }else if(position == 3){
+            return new Galeria();
+        }else if(position == 4){
+            return new SobreNosotros();
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return numTabs;
+    }
+}

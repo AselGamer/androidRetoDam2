6974ba879253664a18194b918e725746982446e9
Iraitz7 <iraitzmauleon@gmail.com>
2023-10-26 21:28:02 +0200

    BottomNavigationView

--- app/src/main/java/com/example/juegalmi/MainActivity.java
+++ app/src/main/java/com/example/juegalmi/MainActivity.java
@@ -1,70 +1,65 @@
 package com.example.juegalmi;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import com.example.juegalmi.adaptadores.PagerAdaptador;
 import com.example.juegalmi.botonesAbajo.Galeria;
 import com.example.juegalmi.botonesAbajo.Productos;
 import com.example.juegalmi.botonesAbajo.Reparaciones;
 import com.example.juegalmi.botonesAbajo.SobreNosotros;
 import com.example.juegalmi.botonesAbajo.Ubicacion;
-import com.example.juegalmi.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.tabs.TabItem;
-import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    //BottomNavigationView bottomNavigationView;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //replaceFragment(new Productos());
-
-        /*bottomNavigationView = findViewById(R.id.bottomNavigationView);
-
-
-
-        bottomNavigationView.setOnItemSelectedListener(item -> {
-
-            if(item.getItemId() == R.id.productos){
-                replaceFragment(new Productos());
-            }else if(item.getItemId() == R.id.reparaciones){
-                replaceFragment(new Reparaciones());
-            }else if(item.getItemId() == R.id.ubicacion){
-                replaceFragment(new Ubicacion());
-            }else if(item.getItemId() == R.id.galeria){
-                replaceFragment(new Galeria());
-            }else if(item.getItemId() == R.id.nosotros){
-                replaceFragment(new SobreNosotros());
-            }
-
-            return true;
-        });*/
-
-        //setupToolbar();
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(this::onItemSelectedListener);
+        //Muestra el fragment inicial cuando la aplicación se inicia
+        bottomNavigationView.setSelectedItemId(R.id.productos);
     }
 
-    /*private void replaceFragment(Fragment fragment){
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frame_layout, fragment);
-        fragmentTransaction.commit();
-    }*/
+    private boolean onItemSelectedListener(MenuItem item) {
+        if(item.getItemId() == R.id.productos) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.contenedor, new Productos())
+                    .commit();
+            return true;
+        }else if(item.getItemId() == R.id.reparaciones) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.contenedor, new Reparaciones())
+                    .commit();
+            return true;
+        }else if(item.getItemId() == R.id.ubicacion) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.contenedor, new Ubicacion())
+                    .commit();
+            return true;
+        }else if(item.getItemId() == R.id.galeria) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.contenedor, new Galeria())
+                    .commit();
+            return true;
+        }else if(item.getItemId() == R.id.nosotros) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.contenedor, new SobreNosotros())
+                    .commit();
+            return true;
+        }
 
-    /*private void setupToolbar() {
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-    }*/
+        return false;
+    }
 }
